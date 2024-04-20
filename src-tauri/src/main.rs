10890850802ff471d34ee9d9f474ace62e29a87a
Noah <noah.fraiture@gmail.com>
2024-04-21 00:42:35 +0200

    [wip] getting position from rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,20 +2,34 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use mouse_position::mouse_position::Mouse;
+use std::cell::RefCell;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![mouse_position])
+    .invoke_handler(tauri::generate_handler![update])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
-#[tauri::command]
-fn mouse_position() -> [i32; 2] {
-  let position = Mouse::get_mouse_position();
+thread_local! {static X: RefCell<i32> = const {RefCell::new(0)}}
+thread_local! {static Y: RefCell<i32> = const {RefCell::new(0)}}
 
+#[tauri::command]
+fn update() -> [i32; 2] {
+  let position = Mouse::get_mouse_position();
   match position {
-    Mouse::Position { x, y } => [x, y],
-    Mouse::Error => [0; 2],
+    Mouse::Position { x, y } => {
+      X.with_borrow_mut(|x_cell| {
+        *x_cell = x;
+      });
+      Y.with_borrow_mut(|y_cell| {
+        *y_cell = y;
+      });
+      [x, y]
+    }
+    Mouse::Error => {
+      println!("Error");
+      [0, 0]
+    }
   }
 }
--- src/lib/Position.svelte
+++ src/lib/Position.svelte
@@ -1,14 +1,19 @@
 <script>
 	import { invoke } from '@tauri-apps/api/tauri';
 
-	let position = [0, 0];
+	let x = 0;
+	let y = 0;
+	let number = 0;
 
 	async function update() {
-		position = await invoke('mouse_position', {});
+		let tuple = await invoke('update', {});
+		x = tuple[0];
+		y = tuple[1];
 	}
 </script>
 
 <div>
 	<button on:click={update}>Update</button>
-	<p>{position}</p>
+	<p>{x} {y}</p>
+	<p>{number}</p>
 </div>
